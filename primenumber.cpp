96431aad6a30464453457cf0377254621edf10fb
ShauryaSarswat <shauryasarswat01@gmail.com>
2021-06-20 20:32:17 +0530

    Uploading the advanced calculator
    Uploading the advanced calculator

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,35 +1,41 @@
 #include<iostream>
+#include<string>
+#include<algorithm>
 using namespace std;
-void prime(int n)
-{
-    int x;
-    bool flag = true;
-    for(int i=2;i<=n/2;i++)
-    {
-        if(n%i==0)
-        {
-            flag=false;
-            break;
-        }
-    }
-    if(flag==true)
-    {
-        cout<<"This "<<n<<" is the prime number ";
-    }
-    else
-    {
-        cout<<"This "<<n<<" is not the prime number ";
-    }
-}
+char runagain;
 int main()
 {
-    int start, ending;
-    cout<<"Enter the user start and the end ";
-    cin>>start>>ending;
-    for(int i=start;i<=ending;i++)
-    {
-        prime(i);
-        cout<<'\n';
-    }
-    return 0;
+   do
+   {
+        float a,b;
+   cout<<"Enter the operands \n";
+   cin>>a>>b;
+   char op;
+   gotoop:
+   cout<<"Enter the operator \n";
+   cin>>op;
+   switch (op)
+   {
+       case '+' : cout<<a<<" + "<<b<<" = "<<a+b<<'\n';
+       break;//string literal
+       case '-' : cout<<a<<" - "<<b<<" = "<<a-b<<'\n';
+       break;
+       case '*' : cout<<a<<" * "<<b<<" = "<<a*b<<'\n'; 
+       break;
+       case '/' : cout<<a<<" / "<<b<<" = "<<a/b<<'\n';
+       break;
+   
+   default:
+     goto gotoop;
+       break;
+   }
+   runagainlabel:
+   
+   cout<<"Enter Y for running again and Enter N for exiting \n";
+   cin>>runagain;
+   if(runagain !='Y' && runagain !='y' && runagain !='N' && runagain !='n')
+   {
+       goto runagainlabel;
+   }
+   } while(runagain == 'Y' || runagain == 'y');//condition match runs again condition false break
 }
